93d1fa0f987bba568577003f99f9808b06029d31
Nathan <nathans.codes@proton.me>
2025-08-17 02:28:03 +0200

    initial info bar, add palette crate for color mixing, change max zoom to 0.5 (50%)

--- src/style.rs
+++ src/style.rs
@@ -1,7 +1,11 @@
 use iced::border::Radius;
-use iced::widget::{button, container, rule};
-use iced::{Border, Color, Shadow, Theme, Vector};
+use iced::gradient::{ColorStop, Linear};
+use iced::widget::slider::Handle;
+use iced::widget::{button, container, rule, slider};
+use iced::{Border, Color, Gradient, Radians, Shadow, Theme, Vector};
 use iced_aw::style::menu_bar;
+use palette::convert::FromColorUnclamped;
+use palette::{IntoColor, Mix, Oklab, Srgba};
 
 use crate::notification::{self, Severity};
 
@@ -293,4 +297,99 @@ pub fn notification_timeout_indicator<'a>(
             fill_mode: rule::FillMode::Percent(timeout / 5.0 * 100.0),
         }
     })
+}
+
+pub fn info_bar(theme: &Theme) -> container::Style {
+    let default = container::dark(theme);
+    let palette = theme.extended_palette();
+
+    let base_color = palette.primary.weak.color;
+
+    let gradient = Gradient::Linear(Linear::new(Radians::PI).add_stops([
+        ColorStop {
+            offset: 0.0,
+            color: base_color.scale_alpha(0.5),
+        },
+        ColorStop {
+            offset: 1.0,
+            color: base_color,
+        },
+    ]));
+
+    let accent_strong = Srgba::from(palette.primary.strong.color.into_linear());
+    let default_text_color = Srgba::from(default.text_color.unwrap_or(Color::WHITE).into_linear());
+
+    let accent_strong = Oklab::from_color_unclamped(accent_strong);
+    let default_text_color = Oklab::from_color_unclamped(default_text_color);
+
+    let text_color: Srgba = accent_strong.mix(default_text_color, 0.8).into_color();
+
+    container::Style {
+        background: Some(gradient.into()),
+        border: Border::default().rounded(10.0).width(1.0).color(base_color),
+        text_color: Some(Color::from(text_color)),
+        shadow: Shadow {
+            blur_radius: 10.0,
+            color: Color::from(text_color),
+            ..Default::default()
+        },
+    }
+}
+
+pub fn info_bar_zoom_slider(theme: &Theme, status: slider::Status) -> slider::Style {
+    let palette = theme.extended_palette();
+
+    let active_color_bright = palette.primary.strong.color;
+    let active_color_dark = palette.primary.weak.color;
+
+    let active_linear_gradient = Linear::new(Radians::PI).add_stops([
+        ColorStop {
+            offset: 0.0,
+            color: active_color_bright,
+        },
+        ColorStop {
+            offset: 1.0,
+            color: active_color_dark,
+        },
+    ]);
+
+    let active_gradient = Gradient::Linear(active_linear_gradient);
+
+    let muted_color = palette.secondary.base.color;
+
+    let muted_gradient = Gradient::Linear(Linear::new(Radians::PI).add_stops([
+        ColorStop {
+            offset: 0.0,
+            color: muted_color.scale_alpha(0.5),
+        },
+        ColorStop {
+            offset: 1.0,
+            color: muted_color,
+        },
+    ]));
+
+    let handle_gradient = match status {
+        slider::Status::Active => active_gradient,
+        slider::Status::Hovered => active_gradient.scale_alpha(0.8),
+        slider::Status::Dragged => Gradient::Linear(
+            Linear::new(0.0).add_stops(active_linear_gradient.stops.iter().filter_map(|s| *s)),
+        ),
+    };
+
+    slider::Style {
+        rail: slider::Rail {
+            backgrounds: (active_gradient.into(), muted_gradient.into()),
+            border: Border::default()
+                .color(active_color_bright)
+                .width(1.0)
+                .rounded(3.0),
+            width: 6.0,
+        },
+        handle: slider::Handle {
+            shape: slider::HandleShape::Circle { radius: 7.0 },
+            background: handle_gradient.into(),
+            border_width: 1.0,
+            border_color: active_color_bright,
+        },
+    }
 }
