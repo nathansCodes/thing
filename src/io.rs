0cc5f05bead366af900d1a902258065eba8c6d63
Nathan <nathans.codes@proton.me>
2025-08-21 02:44:08 +0200

    changed images to store handles to bytes instead of file paths. also changed a whole bunch of asset stuff
    the scroll thing with the dnd provider is broken again for some reason

--- src/io.rs
+++ src/io.rs
@@ -1,9 +1,13 @@
 use std::{
-    io::{Read, Write},
+    collections::HashMap,
+    io::{BufReader, Read, Write},
     path::PathBuf,
 };
 
-use iced::futures::io;
+use file_type::FileType;
+use iced::{futures::io, widget::image};
+
+use crate::assets::{self, Asset};
 
 pub async fn pick_file() -> Result<PathBuf, IOError> {
     let file_handle = rfd::AsyncFileDialog::new()
@@ -44,14 +48,78 @@ pub async fn load(path: PathBuf) -> Result<String, IOError> {
     Ok(data)
 }
 
-pub async fn load_dir(path: PathBuf) -> Result<Vec<PathBuf>, IOError> {
+pub async fn load_dir(path: PathBuf) -> Result<HashMap<String, Asset>, IOError> {
     let dir = std::fs::read_dir(path).map_err(|err| IOError::IO(err.kind()))?;
 
-    let files: Vec<PathBuf> = dir
-        .filter_map(|entry| entry.ok().map(|entry| entry.path()))
+    let assets: HashMap<String, Asset> = dir
+        .filter_map(|entry| {
+            entry.ok().and_then(|entry| {
+                let path = entry.path();
+
+                let mut file = std::fs::File::open(path.clone()).ok()?;
+
+                let mut buffer = Vec::new();
+
+                file.read_to_end(&mut buffer).ok()?;
+
+                let reader = BufReader::new(buffer.as_slice());
+
+                let file_type = FileType::try_from_reader(reader).expect("File type not found!");
+
+                let file_name = path.file_name().unwrap().to_string_lossy().to_string();
+
+                let mut key = path
+                    .parent()
+                    .and_then(|path| {
+                        path.file_name()
+                            .map(|file_name| file_name.to_string_lossy().to_string())
+                    })
+                    .unwrap_or("".to_string())
+                    + "/";
+
+                key.push_str(&file_name);
+
+                file_type
+                    .media_types()
+                    .iter()
+                    .any(|t| t.contains("image"))
+                    .then_some((
+                        key,
+                        Asset::Image(assets::Image {
+                            name: path.file_stem().unwrap().to_string_lossy().to_string(),
+                            file_name,
+                            handle: image::Handle::from_bytes(buffer),
+                        }),
+                    ))
+            })
+        })
         .collect();
 
-    Ok(files)
+    Ok(assets)
+}
+
+pub async fn load_file(path: PathBuf) -> Result<Asset, IOError> {
+    let mut file = std::fs::File::open(path.clone())?;
+
+    let mut buffer = Vec::new();
+
+    file.read_to_end(&mut buffer)?;
+
+    let reader = BufReader::new(buffer.as_slice());
+
+    let file_type = FileType::try_from_reader(reader).expect("File type not found!");
+
+    let file_name = path.file_name().unwrap().to_string_lossy().to_string();
+
+    if file_type.media_types().contains(&"image") {
+        Ok(Asset::Image(assets::Image {
+            name: path.file_stem().unwrap().to_string_lossy().to_string(),
+            file_name,
+            handle: image::Handle::from_bytes(buffer),
+        }))
+    } else {
+        Err(IOError::InvalidAsset)
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -59,6 +127,7 @@ pub enum IOError {
     DialogClosed,
     IO(io::ErrorKind),
     DeserializationFailed(toml::de::Error),
+    InvalidAsset,
 }
 
 impl From<std::io::Error> for IOError {
